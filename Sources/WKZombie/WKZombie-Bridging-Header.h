bde7e0c8309a3ca4c90902946b06992b0fad9f51
David Boyd <baobei@gmx.com>
2020-01-11 11:53:19 -0800

    - Added bridging header

---
+++ Sources/WKZombie/WKZombie-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  WKZombie-Bridging-Header.h
+//
+//  Created by David Boyd on 1/11/20.
+//  Copyright © 2016 Mathias Köhnke. All rights reserved.
+//
+
+#ifndef WKZombie_Bridging_Header_h
+#define WKZombie_Bridging_Header_h
+
+#import "SwiftTryCatch.h"
+
+#endif /* WKZombie_Bridging_Header_h */
